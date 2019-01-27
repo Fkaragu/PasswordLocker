348cf28af11dbb974587ab767587f1be49bdb850
Francis Karagu <fkaragu@gmail.com>
2019-01-27 07:38:35 +0300

    Modified the add user functionality

--- app.py
+++ app.py
@@ -1,2 +1,132 @@
 #!/usr/bin/env python3.6
-from userClass import User
+from user import User
+
+def create_user(fname,lname,phone,email):
+
+    new_user = User(fname,lname,phone,email)
+    return new_user
+
+def save_users(user):
+
+    user.save_user()
+
+def del_user(user):
+
+    user.delete_user()
+
+def find_user(number):
+
+    return User.find_by_number(number)
+
+def check_existing_users(number):
+
+    return User.user_exist(number)
+
+def display_users():
+
+    return User.display_users()
+
+def test_copy_email():
+
+    return User.copy_email()
+
+def main():
+
+    print("Hello Welcome to your user list. What is your name?")
+
+    user_name = input()
+
+    print(f"Hello {user_name}. what would you like to do")
+
+    print('\n')
+
+    while True:
+
+        print("Use these short codes : cc - create a new user, dc - display users, fc -find a user, ex -exit the user list ")
+
+        short_code = input().lower()
+
+        if short_code == 'cc':
+
+            print("New User")
+            print("-"*10)
+
+            print ("First name ....")
+            f_name = input()
+
+            print("Last name ...")
+            l_name = input()
+
+            print("Phone number ...")
+            p_number = input()
+
+            print("Email address ...")
+            e_address = input()
+
+            save_users(create_user(f_name,l_name,p_number,e_address))
+
+            print ('\n')
+
+            print(f"New User {f_name} {l_name} created")
+
+            print ('\n')
+
+        elif short_code == 'dc':
+
+            if display_users():
+
+                print("Here is a list of all your users")
+
+                print('\n')
+
+                for user in display_users():
+
+                    print(f"{user.first_name} {user.last_name} .....{user.phone_number}")
+
+                print('\n')
+
+            else:
+
+                print('\n')
+
+                print("You dont seem to have any users saved yet")
+
+                print('\n')
+
+        elif short_code == 'fc':
+
+            print("Enter the number you want to search for")
+
+            search_number = input()
+
+            if check_existing_users(search_number):
+
+                search_user = find_user(search_number)
+
+                print(f"{search_user.first_name} {search_user.last_name}")
+
+                print('-' * 20)
+
+
+                print(f"Phone number.......{search_user.phone_number}")
+
+                print(f"Email address.......{search_user.email}")
+
+            else:
+
+                 print("That user does not exist")
+
+        elif short_code == "ex":
+
+            print("Bye .......")
+
+            break
+
+        else:
+
+            print("I really didn't get that. Please use the short codes")
+
+
+if __name__ == '__main__':
+
+    main()
---
+++ user.py
@@ -0,0 +1,54 @@
+import pyperclip
+class User:
+    """
+    Class that generates new instances of users.
+    """
+    pass
+
+    user_list = [] # Empty user listz
+
+    def save_user(self):
+
+        User.user_list.append(self)
+
+    def delete_user(self):
+
+        User.user_list.remove(self)
+
+    @classmethod
+    def find_by_number(cls,number):
+
+        for user in cls.user_list:
+            if user.phone_number == number:
+                return user
+
+    @classmethod
+    def user_exist(cls,number):
+
+        for user in cls.user_list:
+            if user.phone_number == number:
+                    return True
+
+        return False
+
+    @classmethod
+    def display_users(cls):
+
+        return cls.user_list
+
+    @classmethod
+    def copy_email(cls,number):
+        user_found = User.find_by_number(number)
+        pyperclip.copy(user_found.email)
+
+
+
+
+    def __init__(self,first_name,last_name,phone_number,email):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
